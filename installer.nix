9c1c13f1406fc1eed8d8f185f6a42abe1c795b60
Ethan Keats <ethankkeats@gmail.com>
2026-05-21 13:24:43 -0400

    fixed installer not having a hardware configuration

--- installer.nix
+++ installer.nix
@@ -40,7 +40,8 @@
       # Install Nixos 
       mkdir -p /mnt/etc/nixos 
       cp -r /iso/simpleKiosk /mnt/etc/nixos/simpleKiosk # make a local copy of the entire flake on the target machine
-      nixos-generate-config --root /mnt --dir /mnt/etc/nixos # generate the dan gum hardware-configuration.nix on the target machine 
+      nixos-generate-config --root /mnt --dir /mnt/etc/nixos # generate the dan gum hardware-configuration.nix on the target machine
+      cp /mnt/etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix # copy the hardware configuration to the installer's /etc/nixos/ too so nix-install can buld.
       nixos-install --flake github:BrettZolstick/simpleKiosk#kiosk --no-root-passwd --impure # install from the local flake
 
       # Reboot
