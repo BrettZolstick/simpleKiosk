1f4c93b09d00a096653ca70f7be457ea06829a7d
Ethan <crazyeman83@gmail.com>
2026-05-23 04:10:00 +0000

    trying foot black screen instead of swaylock

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@ let
     default_floating_border none
     seat * hide_cursor 3000
     exec ${startKiosk}
-    exec swayidle -w timeout ${idleTimeout} '${blackScreen}' resume 'pkill chromium; pkill -f black-screen'
+    exec swayidle -w timeout ${idleTimeout} '${blackScreen}' resume 'pkill chromium; pkill -f foot'
   '';
 
 
