472f7b3df274df28810934355a91bb90957511fa
Ethan <crazyeman83@gmail.com>
2026-05-27 15:43:10 +0000

    make garbage go away on boot

--- kioskConfig/advancedConfig.nix
+++ kioskConfig/advancedConfig.nix
@@ -44,12 +44,14 @@ in
     efiSysMountPoint = "/boot";
   };
   boot.kernelParams = [
-    "quiet"
-    "splash"
-    "loglevel=3"
-    "udev.log_level=3"
-    "rd.udev.log_level=3"
-    "vt.global_cursor_default=0"
+  "quiet"
+  "splash"
+  "boot.shell_on_fail"
+  "loglevel=0"
+  "rd.systemd.show_status=false"
+  "rd.udev.log_level=0"
+  "udev.log_priority=0"
+  "vt.global_cursor_default=0"
   ];
   boot.plymouth = {
     enable = true;
